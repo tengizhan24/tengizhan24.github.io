f6532a88557c8d63be0b80bf4b5d3263a6a10813
tengizhan24 <tturusbekovic@gmail.com>
2020-10-13 17:16:25 +0600

    Ad

---
+++ build/precache-manifest.67908d34a29279e1f9467c5261a8e1c0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51a11e52348752ac11e04e64f4870ab0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f87c5317faa078b95f58",
+    "url": "/static/css/main.b5a6bd58.chunk.css"
+  },
+  {
+    "revision": "b911d4d8e46560afd077",
+    "url": "/static/js/2.1bc31245.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1bc31245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f87c5317faa078b95f58",
+    "url": "/static/js/main.91c3c7c6.chunk.js"
+  },
+  {
+    "revision": "9cf1ae8e70b01b27d494",
+    "url": "/static/js/runtime-main.7e871748.js"
+  }
+]);
